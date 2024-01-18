07c0c1865ba66c7b20900d3c5b5155b4a9154d5b
nayonez <144882233+nayonez@users.noreply.github.com>
2024-01-19 01:23:41 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 132 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = 543;
+        System.out.println(a-b);
+    }
+}
