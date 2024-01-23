6bafc5a9760b1927da2237d27af3bea34095aa41
nayonez <144882233+nayonez@users.noreply.github.com>
2024-01-23 14:22:45 +0900

    [Silver V] Title: 색종이, Time: 128 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int[][] intArr = new int[100][100]; // 0으로 초기화
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    intArr[j][k] = 1;
+                }
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (intArr[i][j] == 1) {
+                    result += 1;
+                }
+            }
+        }
+        
+        System.out.println(result);
+
+    }
+}
