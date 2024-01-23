db925651957b1780238c4fbc441b25f3a73052cf
nayonez <144882233+nayonez@users.noreply.github.com>
2024-01-23 13:55:18 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 128 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < N-i; j++) {
+                sb.append(" ");
+            }
+            for (int k = 0; k < i; k++) {
+                sb.append("*");
+            } sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
