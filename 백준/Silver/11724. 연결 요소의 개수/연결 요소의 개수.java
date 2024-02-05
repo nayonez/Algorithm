54c8d8c2e1c0da0291d2c59074c18830c7738b06
nayonez <144882233+nayonez@users.noreply.github.com>
2024-02-06 06:25:27 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 524 ms, Memory: 114004 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int connect;
+    static int N;
+    static boolean[] visited;
+    static boolean[][] graph;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        graph = new boolean[N][N];
+        visited = new boolean[N];
+        connect = 0;
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken()) - 1;
+            int v = Integer.parseInt(st.nextToken()) - 1;
+            graph[u][v] = graph[v][u] = true;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                bfs(i);
+                connect++;
+            }
+        }
+        bfs(0);
+        System.out.println(connect);
+    }
+
+    private static void bfs(int start) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        visited[start] = true;
+        queue.offer(start);
+
+        while (!queue.isEmpty()) {
+            int temp = queue.poll();
+            for (int i = 0; i < N; i++) {
+                if (visited[i]==false && graph[temp][i] == true) {
+                    queue.offer(i);
+                    visited[i] = true;
+                }
+            }
+        }
+    }
+}
