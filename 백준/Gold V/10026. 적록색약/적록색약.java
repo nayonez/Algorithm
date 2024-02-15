ff6dc763109b2b3a230d6c24bba45630d7980d3f
nayonez <144882233+nayonez@users.noreply.github.com>
2024-02-15 13:40:12 +0900

    [Gold V] Title: 적록색약, Time: 136 ms, Memory: 14988 KB -BaekjoonHub

--- 백준/Gold V/10026. 적록색약/적록색약.java
+++ 백준/Gold V/10026. 적록색약/적록색약.java
@@ -4,34 +4,44 @@ import java.util.ArrayDeque;
 import java.util.Queue;
 
 public class Main {
+    static int[][] deltas = {{-1,0},{1,0},{0,-1},{0,1}};
+    static int N, count;
     static char[][] map;
     static boolean[][] visited;
-    static int N;
 
     public static void main(String[] args) throws Exception {
-        int count = 0;
         StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
+        count = 0;
         map = new char[N][N];
         visited = new boolean[N][N];
+
         for (int i = 0; i < N; i++) {
             map[i] = br.readLine().toCharArray();
         }
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (visited[i][j]) continue;
-                bfs(i, j);
+        // 적록색약이 아닌 사람
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                if (visited[r][c]) continue;
+                bfs(r,c,map[r][c]);
                 count++;
             }
         } sb.append(count).append(" ");
 
-        visited = new boolean[N][N];
+        // 적록색약인 사람
         count = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (visited[i][j]) continue;
-                bfs2(i, j);
+        visited = new boolean[N][N];
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                if (map[r][c] == 'R')
+                    map[r][c] = 'G';
+            }
+        }
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                if (visited[r][c]) continue;
+                bfs(r,c,map[r][c]);
                 count++;
             }
         } sb.append(count);
@@ -39,60 +49,21 @@ public class Main {
         System.out.println(sb);
     }
 
-    public static void bfs(int r, int c) {
-        int[][] deltas = {{-1,0},{1,0},{0,-1},{0,1}};
-        Queue<Node> queue = new ArrayDeque<>();
-        queue.offer(new Node(r, c));
+    private static void bfs(int r, int c, char color) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[] {r, c});
         visited[r][c] = true;
-        // 사방탐색
         while (!queue.isEmpty()) {
-
-            Node node = queue.poll();
+            int[] temp = queue.poll();
             for (int d = 0; d < 4; d++) {
-                int nr = node.r + deltas[d][0];
-                int nc = node.c + deltas[d][1];
-                if (nr < 0 || nr >= N || nc < 0 || nc >=N) continue;
+                int nr = temp[0] + deltas[d][0];
+                int nc = temp[1] + deltas[d][1];
+                if (nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
                 if (visited[nr][nc]) continue;
-                if (map[r][c]!=map[nr][nc]) continue;
-
-                queue.offer(new Node(nr,nc));
+                if (color!=map[nr][nc]) continue;
+                queue.offer(new int[] {nr, nc});
                 visited[nr][nc] = true;
             }
-        }
-    }
-
-    public static void bfs2(int r, int c) {
-        int[][] deltas = {{-1,0},{1,0},{0,-1},{0,1}};
-        Queue<Node> queue = new ArrayDeque<>();
-        queue.offer(new Node(r, c));
-        visited[r][c] = true;
-        // 사방탐색
-        while (!queue.isEmpty()) {
-
-            Node node = queue.poll();
-            for (int d = 0; d < 4; d++) {
-                int nr = node.r + deltas[d][0];
-                int nc = node.c + deltas[d][1];
-                if (nr < 0 || nr >= N || nc < 0 || nc >=N) continue;
-                if (visited[nr][nc]) continue;
-                if (map[r][c] == 'B') {
-                    if (map[r][c]!=map[nr][nc]) continue;
-                } else {
-                    if (map[nr][nc]=='B') continue;
-                }
-
-                queue.offer(new Node(nr,nc));
-                visited[nr][nc] = true;
-            }
-        }
-    }
-
-    public static class Node {
-        int r, c;
-
-        public Node(int r, int c) {
-            this.r = r;
-            this.c = c;
         }
     }
 }
