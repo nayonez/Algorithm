f346fa484701330cea6866046c3516576b52079f
nayonez <144882233+nayonez@users.noreply.github.com>
2024-02-15 15:14:46 +0900

    [Gold V] Title: 농장 관리, Time: 156 ms, Memory: 15012 KB -BaekjoonHub

--- 백준/Gold V/1245. 농장 관리/농장 관리.java
+++ 백준/Gold V/1245. 농장 관리/농장 관리.java
@@ -29,32 +29,27 @@ public class Main {
         for (int r = 0; r < N; r++) {
             for (int c = 0; c < M; c++) {
                 if (visited[r][c]) continue;
-                bfs(r, c, map[r][c]);
-                if (flag)
-                count++;
+                flag = true;
+                //                bfs(r, c, map[r][c]);
+                dfs(r, c, map[r][c]);
+                if (flag) count++;
             }
         }
         System.out.println(count);
     }
-
-    private static void bfs(int r, int c, int height) {
-        flag = true;
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.offer(new int[] {r, c});
+    
+    private static void dfs(int r, int c, int height) {
         visited[r][c] = true;
-        while (!queue.isEmpty()) {
-            int[] temp = queue.poll();
-            for (int d = 0; d < 8; d++) {
-                int nr = temp[0] + deltas[d][0];
-                int nc = temp[1] + deltas[d][1];
-                if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
-                if (map[nr][nc] > height) // 인접한 게 현재높이보다 클때->산봉우리가 아님
-                    flag = false;
-                if (visited[nr][nc]) continue;
-                if (map[nr][nc]!=height) continue;
-                queue.offer(new int[] {nr, nc});
-                visited[nr][nc] = true;
-            }
+        for (int d = 0; d < 8; d++) {
+            int nr = r + deltas[d][0];
+            int nc = c + deltas[d][1];
+            if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
+            if (map[nr][nc] > height) // 인접한 게 현재높이보다 클때->산봉우리가 아님
+                flag = false;
+            if (visited[nr][nc]) continue;
+            if (map[nr][nc]!=height) continue;
+            visited[nr][nc] = true;
+            dfs(nr, nc, height);
         }
     }
 }
