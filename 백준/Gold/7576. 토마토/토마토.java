6c75743de39616d0d0e15ad0b0684dace685ff34
nayonez <144882233+nayonez@users.noreply.github.com>
2024-02-20 03:20:48 +0900

    [Gold V] Title: 토마토, Time: 664 ms, Memory: 103396 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -5,65 +5,63 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static Queue<Node> queue = new ArrayDeque<>();
-    static int deltas[][] = {{-1, 0}, {1, 0}, {0, -1}, {0,1}};
-    static int count, N, M;
-    static boolean[][] visited;
+    static int[][] deltas = {{-1,0},{1,0},{0,-1},{0,1}};
     static int[][] map;
+    static int count, N, M, day;
+    static Queue<Node> queue = new ArrayDeque<>();
 
-    public static void main (String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken()); // 가로칸의 수 = 열 = C
-        N = Integer.parseInt(st.nextToken()); // 세로칸의 수 = 행 = R
         count = 0;
+        M = Integer.parseInt(st.nextToken()); // 열
+        N = Integer.parseInt(st.nextToken()); // 행
         map = new int[N][M];
-        visited = new boolean[N][M];
-        for (int r = 0; r < N; r++) {
+
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int c = 0; c < M; c++) {
-                map[r][c] = Integer.parseInt(st.nextToken()); // 1 익음, 0 안익음, -1 토마토없음
-                if (map[r][c] == 1) {
-                    queue.offer(new Node(r,c));
-                    visited[r][c] = true;
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 1) {
+                    queue.offer(new Node(i,j));
                 }
             }
         }
         bfs();
-        for (int r = 0; r < N && count != 0 ; r++) {
+        day = 0;
+        System.out.println(ans());
+
+    }
+    private static int ans() {
+        for (int r = 0; r < N; r++) {
             for (int c = 0; c < M; c++) {
+                if (map[r][c] > 0) {
+                    day = Math.max(day, map[r][c]);
+                }
                 if (map[r][c] == 0) {
-                    count = 0;
-                    break;
+                    return -1; // 안익은 토마토가 한개라도 있으면 -1
                 }
             }
-        }
-        System.out.println(count-1);
+        } return day - 1;
     }
-
     private static void bfs() {
         while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                Node node = queue.poll();
-                // 사방탐색
-                for (int d = 0; d < 4; d++) {
-                    int nr = node.r + deltas[d][0];
-                    int nc = node.c + deltas[d][1];
-                    if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
-                    if (visited[nr][nc]) continue;
-                    if (map[nr][nc]==-1) continue;
-                    queue.offer(new Node(nr,nc));
-                    visited[nr][nc] = true;
-                    map[nr][nc] = 1;
+            Node node = queue.poll();
+            for (int d = 0; d < 4; d++) {
+                int nr = node.r + deltas[d][0];
+                int nc = node.c + deltas[d][1];
+                if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
+                if (map[nr][nc] == -1) continue;
+                if (map[nr][nc] == 0) {
+                    map[nr][nc] = map[node.r][node.c] + 1;
+                    queue.offer(new Node(nr, nc));
                 }
-            } count++;
+            }
         }
     }
 
     private static class Node {
         int r, c;
-
         public Node(int r, int c) {
             this.r = r;
             this.c = c;
