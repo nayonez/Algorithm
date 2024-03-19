481386bd6f598dcf261b858c693116cb7bcee2e2
nayonez <144882233+nayonez@users.noreply.github.com>
2024-03-20 04:09:13 +0900

    [Silver I] Title: RGB거리, Time: 136 ms, Memory: 14620 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -5,19 +5,20 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine()); // 집의 수
+        int N = Integer.parseInt(br.readLine());
         int[][] dp = new int[N][3];
-        // 비용 입력받기
+        
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 3; j++) { // r g b 순서로 3개
+            for (int j = 0; j < 3; j++) {
                 dp[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+        
         for (int i = 1; i < N; i++) {
-                dp[i][0] += Math.min(dp[i-1][1], dp[i-1][2]);
-                dp[i][1] += Math.min(dp[i-1][0], dp[i-1][2]);
-                dp[i][2] += Math.min(dp[i-1][0], dp[i-1][1]);
+            dp[i][0] += Math.min(dp[i-1][1], dp[i-1][2]);
+            dp[i][1] += Math.min(dp[i-1][0], dp[i-1][2]);
+            dp[i][2] += Math.min(dp[i-1][0], dp[i-1][1]);
         }
         System.out.println(Math.min(dp[N-1][0], Math.min(dp[N-1][1], dp[N-1][2])));
     }
