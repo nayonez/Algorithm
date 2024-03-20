0325efd8071ba3d0c7da0cde3e4c3b84060c6e9d
nayonez <144882233+nayonez@users.noreply.github.com>
2024-03-20 14:05:52 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 148 ms, Memory: 14824 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+// https://www.acmicpc.net/problem/11053 - 가장 긴 증가하는 부분 수열
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+        int ans = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1; // 자기 자신 포함! 최솟값은 길이가 다 1이다.
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+
+                }
+            } ans = Math.max(ans, dp[i]);
+        }
+        System.out.println(ans);
+    }
+}
