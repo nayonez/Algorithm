3da4a91f465f332c20fb2cfe890d1476433c4fa8
nayonez <144882233+nayonez@users.noreply.github.com>
2024-04-22 15:31:37 +0900

    [level 3] Title: 표 편집, Time: 788.79 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,34 +1,18 @@
-import java.util.*;
 class Solution {
     public String solution(int n, int k, String[] cmd) {
-        StringBuilder sb = new StringBuilder();
-        Stack<Integer> stack = new Stack<>();
-        for (String s : cmd) {
-            switch (s.charAt(0)) {
-                case 'U' : 
-                    k -= Integer.parseInt(s.split(" ")[1]);
-                    break;
-                
-                case 'D' : 
-                    k += Integer.parseInt(s.split(" ")[1]);
-                    break;
-                
-                case 'C' : 
-                    stack.push(k); // 삭제된 행의 인덱스를 스택에 추가
-                    n--;
-                    if (k==n) k--; // 마지막 행이라면 선택된 행 하나 위로 변경!
-                    break;
-                
-                case 'Z' : 
-                    n++;
-                    if (stack.pop() <= k) k++;
-                    break;
-            } // switch 
-      } // for
-        sb.append("O".repeat(Math.max(0, n)));
-            while (!stack.isEmpty()) {
-                sb.insert(stack.pop(), "X");
+        int[] map = new int[n]; // 0으로 초기화
+        StringTokenizer st;
+        
+        for (int i = 0; i < cmd.length(); i++) {
+            st = new StringTokenizer(cmd[i]);
+            String temp = st.nextToken();
+            switch (temp) {
+                case : "U" {
+                    Integer.parseInt(st.nextToken());
+                }
             }
-        return sb.toString();
+        }
+        String answer = "";
+        return answer;
     }
 }
