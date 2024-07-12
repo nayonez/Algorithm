47a642228adfb31f917dd621a4ad938205d37e01
nayonez <144882233+nayonez@users.noreply.github.com>
2024-07-12 21:20:41 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 176 ms, Memory: 14892 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,16 +1,15 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-       
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
-        for(int i = 1; i <= N; i++) {
-            for(int j = 0; j < i; j++) {
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                // i번만큼 별을 찍어야함
                 System.out.print("*");
-            }
-            System.out.println("");
+            } System.out.println("");
         }
     }
 }
