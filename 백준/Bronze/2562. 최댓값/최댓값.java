ef40f0f80e9bb90c59aa51d03dfc5d3167a06f6d
nayonez <144882233+nayonez@users.noreply.github.com>
2024-07-13 19:14:15 +0900

    [Bronze III] Title: 최댓값, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,23 +1,22 @@
+import java.util.*;
 import java.io.*;
-import java.io.*;
-
 public class Main {
     public static void main(String[] args) throws Exception {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int[] numbers = new int[9];
-        int max = Integer.MIN_VALUE;
-        int result = 0;
-
-        for(int i = 0; i < 9; i++ ) {
-            numbers[i] = Integer.parseInt(br.readLine());
-            if(max < numbers[i]) {
-                max = numbers[i];
-                result = i + 1;
+        int[] originalArr = new int[9];
+        int[] sortArr = new int[9];
+        for (int i = 0; i < 9; i++) {
+            sortArr[i] = originalArr[i] = Integer.parseInt(br.readLine());                        
+        }
+        Arrays.sort(sortArr);
+        int max = sortArr[8];
+        for (int i = 0; i < 9; i++) {
+            if (originalArr[i] == max) {
+                max = i+1;
+                break;
             }
         }
+        System.out.println(sortArr[8]);
         System.out.println(max);
-        System.out.println(result);
     }
 }
