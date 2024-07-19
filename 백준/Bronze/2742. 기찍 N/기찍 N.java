9615072695fa55039825951f702a23f8d34b4666
nayonez <144882233+nayonez@users.noreply.github.com>
2024-07-19 15:34:28 +0900

    [Bronze IV] Title: 기찍 N, Time: 232 ms, Memory: 18056 KB -BaekjoonHub

--- 백준/Bronze/2742. 기찍 N/기찍 N.java
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -3,9 +3,11 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
         for (int i = n; i > 0; i--) {
-            System.out.println(i);
+            sb.append(i).append("\n");
         }
+        System.out.print(sb);
     }
 }
