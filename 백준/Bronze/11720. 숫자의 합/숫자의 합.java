d3db9fa27a73d7f358adfbdae4e8c0b1a1592694
nayonez <144882233+nayonez@users.noreply.github.com>
2024-07-19 15:21:11 +0900

    [Bronze IV] Title: 숫자의 합, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -1,15 +1,19 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        char[] c = new char[N];
+        int n = Integer.parseInt(br.readLine());
+        // 입력에 공백이 없기 때문에 charArr로 받음
+        char[] charArr = new char[n];
+        charArr = br.readLine().toCharArray();
         int sum = 0;
-        c = br.readLine().toCharArray();
-        for(int i = 0; i < N; i++) {
-            sum += c[i]-'0';
+
+        for (int i = 0; i < n; i++) {
+            // char는 숫자연산이 안되니까 - '0' 연산해서 숫자로 만들어주기
+            if (charArr[i] - '0' > 0) {
+                sum += charArr[i] - '0';
+            }
         }
         System.out.println(sum);
     }
